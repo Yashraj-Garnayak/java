39e137fe4563f5afe15e5d5fc871de7836a0f7a6
Yashraj-Garnayak <yash.disraj@gmail.com>
2024-12-10 05:00:47 +0530

    variables and premitiv data types

---
+++ data_type.java
@@ -0,0 +1,27 @@
+class data_types{
+
+    public static void main (String a[]){
+
+    #integers 
+    byte a = 13;
+    short b = 1345;
+    int c = 14567;
+    long d = 123456543212345l;
+
+    # decimal points
+    float e = 15.54f;
+    double f = 1456.53434243;
+
+    # char
+    char g = 'a';
+
+    # boolean values true or false
+    boolean h = true;
+    boolean i = false;
+    # bool could also be in zero = false and one = true
+    boolean j = 1;
+    boolean k = 0;
+
+
+    }
+}
