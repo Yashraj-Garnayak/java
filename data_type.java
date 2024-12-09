033e93bb15c95316a56bcfa870f0f17fa432bbdc
Yashraj-Garnayak <yash.disraj@gmail.com>
2024-12-10 05:03:18 +0530

    primitive data types

--- data_type.java
+++ data_type.java
@@ -2,23 +2,23 @@ class data_types{
 
     public static void main (String a[]){
 
-    #integers 
+    //integers 
     byte a = 13;
     short b = 1345;
     int c = 14567;
     long d = 123456543212345l;
 
-    # decimal points
+    // decimal points
     float e = 15.54f;
     double f = 1456.53434243;
 
-    # char
+    // char
     char g = 'a';
 
-    # boolean values true or false
+    // boolean values true or false
     boolean h = true;
     boolean i = false;
-    # bool could also be in zero = false and one = true
+    // bool could also be in zero = false and one = true
     boolean j = 1;
     boolean k = 0;
 
