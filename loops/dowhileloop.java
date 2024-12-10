7d60732a1a9af7df348a2df37d4590de5ade2bb4
Yashraj-Garnayak <yash.disraj@gmail.com>
2024-12-10 08:34:23 +0530

    all types of loop

---
+++ loops/dowhileloop.java
@@ -0,0 +1,14 @@
+class loops{
+    public static void main(String a[]){
+        // ea same hi hai while loop ki tarha lekin 
+        //iss loop mai pehle whle ke andar ja kar ak bar execute karta hai use baad hi DO condition check karta hau!!
+         int count = 1;
+        do {
+            System.out.println("hi hi ");
+            count++;
+        }while(count <100);
+
+        // ak last message bhi print kara dete hna 
+        System.out.println("Khatm tata by.. by..");
+    }
+}
---
+++ loops/whileLoop.java
@@ -0,0 +1,14 @@
+class hello{
+    public static void main(String a[]){
+        // halo ak whiel loop mai hi hi print karate hna
+        // create a couter
+        int count = 1;
+        while (count <100){
+            System.out.println("hi hi ");
+            count++;
+        }
+
+        // ak last message bhi print kara dete hna 
+        System.out.println("Khatm tata by.. by..");
+    }
+}
